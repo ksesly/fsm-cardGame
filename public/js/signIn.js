47f06058e2676a3e910ecab9365e25382ca1ba0f
ksesly <ks.you.04@gmail.com>
2023-09-26 15:50:41 +0300

    fix: normal sign-in, sign-up

---
+++ public/js/signIn.js
@@ -0,0 +1,63 @@
+function createSignIn() {
+    const mySignIn = document.querySelector('.sign-in');
+    const signInForm = mySignIn.appendChild(document.createElement("form"));
+    signInForm.className = "sign-in-form";
+    const signInLabel = signInForm.appendChild(document.createElement("div"));
+    signInLabel.textContent = "sign in";
+    signInLabel.className = "sign-in-lable";
+    const signInDiv = signInForm.appendChild(document.createElement("div"));
+    signInDiv.className = "sign-in-div";
+    const signInArray = ["Login or Email", "Password"];
+    signInArray.forEach(i => {
+        const divField = document.createElement("div");
+        divField.className = "div-input-field";
+        const labelForField = document.createElement("label");
+        labelForField.textContent = i;
+        const inputField = document.createElement("input");
+        const attributes = {
+            name: i.toLowerCase(),
+            type: i.toLowerCase(),
+            class: "input-field", 
+            id: i.toLowerCase() + "-input"
+        }
+        Object.keys(attributes).forEach(attr => {
+            if (attributes[attr] === "login" && inputField === "Login or Email")
+                inputField.setAttribute("type", "text");
+            else
+                inputField.setAttribute(attr, attributes[attr]);
+        });
+        divField.appendChild(labelForField);
+        divField.appendChild(inputField);
+        signInDiv.appendChild(divField);
+    });
+
+    const signInButton = document.createElement("button");
+    signInButton.setAttribute("type", "submit");
+    signInButton.className = "sign-in-button";
+    signInButton.textContent = "sign in"
+    signInDiv.appendChild(signInButton);
+
+    const signUpLink = document.createElement('a');
+    signUpLink.setAttribute("href", "./signUp.html");
+    signUpLink.textContent = "do not have an account?";
+
+    signInDiv.appendChild(signUpLink);
+    
+}
+
+createSignIn();
+
+// const buttons = [...document.getElementsByTagName('button')];
+// buttons.forEach(i => {
+//     i.addEventListener("click", () => {
+//         if (i.id === "start-button")
+//             location.replace("./createBattle.html");
+//         else if (i.id === "tutorial-button")
+//             location.replace("./tutorial.html");
+//         else if (i.id === "settings-button")
+//             location.replace("./settings.html");
+//         else if (i.id === "exit-button")
+//             location.replace("./exit.html");
+    
+//     })
+// })
