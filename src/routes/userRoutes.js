dec2693e8a89f0bc6c7124c40d4645f4d25ea7cd
Sheeseburger <kekymber@gmail.com>
2023-10-03 21:26:34 +0300

    created route for getting user

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const catchAsync = require('../utils/catchAsync');
 const router = express.Router();
 
 router
@@ -14,5 +15,18 @@ router
 	.get((req, res) => {
 		res.sendFile('signIn.html', { root: './public/views/' });
 	});
+router.route('/getUser/:id').get(
+	catchAsync(async (req, res) => {
+		const userId = req.params.id;
 
+		const user = await User.findByPk(userId, {
+			attributes: { exclude: ['password'] },
+		});
+
+		if (!user) {
+			return res.status(404).json({ message: 'User not found' });
+		}
+		res.status(200).json({ user });
+	})
+);
 module.exports = router;
